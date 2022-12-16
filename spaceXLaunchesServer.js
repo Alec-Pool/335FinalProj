1396e4c3f4200ba75ea9ffdc7afa962693179f69
Alec Pool <alec.pool@gmail.com>
2022-12-16 00:49:29 -0500

    some other changes

--- spaceXLaunchesServer.js
+++ spaceXLaunchesServer.js
@@ -107,10 +107,10 @@ const username = process.env.MONGO_DB_USERNAME;
 const password = process.env.MONGO_DB_PASSWORD;
 
 const databaseAndCollection = {db: process.env.MONGO_DB_NAME, collection: process.env.MONGO_COLLECTION};
-/*
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${username}:${password}@cluster0.zsgbhxu.mongodb.net/?retryWrites=true&w=majority`
-   
+/*   
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 let launchData = "";
